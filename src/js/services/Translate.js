886ae64155923d50b6849f7adfdc1a9cda4fae75
Dominick Peluso <peluso.dominick@gmail.com>
2015-11-09 20:36:46 -0500

    Begin translator service

--- src/js/services/Translate.js
+++ src/js/services/Translate.js
@@ -1,5 +1,18 @@
 angular.module('drive_zombify')
-    .service('Translate', ['Parameters', function(Parameters) {
-
+    .service('Translator', ['$http', 'Parameters', function($http, Parameters){
         var svc = this;
-}]);
+
+        var service_url = Parameters.translation_api_url;
+
+        svc.translate = function(query, callback){
+            $http.get(service_url + query)
+                .then(function(response) {
+                    svc.users = response.data;
+                });
+            if(callback){
+                callback();
+            }
+        };
+
+
+    }]);
