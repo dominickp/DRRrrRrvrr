9714d16c5e02e5bad50f4bd4d52dd127ff5af625
Dominick Peluso <peluso.dominick@gmail.com>
2015-11-11 17:21:04 -0500

    Use link to put logic into directive

--- src/js/directives/oauth_button.js
+++ src/js/directives/oauth_button.js
@@ -1,7 +1,18 @@
 angular.module('drive_zombify')
-    .directive('oauthButton', [function(){
+    .directive('oauthButton', ['GoogleDrive', function(GoogleDrive){
         return {
             scope: {
+            },
+            controller: function($scope, $element){
+
+            },
+            link: function($scope, $element){
+
+                $scope.beginDriveAuthorization = function(){
+                    console.log('Hello');
+                    GoogleDrive.handleAuthClick();
+                };
+
             },
             templateUrl: 'templates/oauth_button.html'
         };
