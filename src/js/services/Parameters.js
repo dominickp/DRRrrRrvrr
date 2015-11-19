f3f45223a79125b788695732088d5bed96dd997f
Dominick Peluso <peluso.dominick@gmail.com>
2015-11-19 18:17:26 -0500

    Delete Parameters.js

--- src/js/services/Parameters.js
+++
@@ -1,5 +0,0 @@
-angular.module('drive_zombify').value('Parameters', {
-    client_id: '851175748720-79i3sbft9cbhr27o9bmdp5s4cnof3puv.apps.googleusercontent.com',
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-    translation_api_url: 'http://ancient-anchorage-9224.herokuapp.com/zombify?q='
-});
